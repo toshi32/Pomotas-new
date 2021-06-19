dd74b16426776b269d19320447749a1535c236ac
toshi32 <viva.la.vida0522@gmail.com>
2021-06-20 03:05:39 +0900

    追加: Task機能 View設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_19_153757) do
+ActiveRecord::Schema.define(version: 2021_06_19_172829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content"
+    t.date "time_limit", default: "2021-06-20", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
