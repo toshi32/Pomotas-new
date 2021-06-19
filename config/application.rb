dd93978bd033c091fd668776d5b18e07a9ea6244
toshi32 <viva.la.vida0522@gmail.com>
2021-06-20 01:28:59 +0900

    追加: deviseの日本語化

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Pomotas
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.assets false
       g.skip_routes false
