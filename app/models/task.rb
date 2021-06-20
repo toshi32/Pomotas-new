c71ee2a1f898da64b87601df4664ebf5fee18ce1
toshi32 <viva.la.vida0522@gmail.com>
2021-06-20 14:37:58 +0900

    変更: Task機能 ラベル表示微調整

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { in: 1..100 }
   validates :content, presence: true, length: { in: 1..100 }
-  validates :limit, presence: true
+  validates :time_limit, presence: true
   has_many :labellings, dependent: :destroy
   has_many :labels, through: :labellings
 end
