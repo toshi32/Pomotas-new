285c615b2cbc58e33e7cc0a327b05d83d8407b6a
toshi32 <viva.la.vida0522@gmail.com>
2021-06-20 13:50:58 +0900

    追加: Task機能 タスクとラベルのアソシエーション設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { in: 1..100 }
   validates :content, presence: true, length: { in: 1..100 }
+  validates :limit, presence: true
+  has_many :labellings, dependent: :destroy
+  has_many :labels, through: :labellings
 end
