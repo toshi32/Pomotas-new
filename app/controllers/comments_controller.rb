b60381665beff0724d3ed967d94a1946755667e4
toshi32 <viva.la.vida0522@gmail.com>
2021-06-22 23:07:00 +0900

    追加: コメントコントローラーの変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,19 @@
 class CommentsController < ApplicationController
   def create
+    @task = Task.find(params[:task_id])
+    @comment = @blog.comments.build(comment_params)
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to task_path(@task) }
+      else
+        format.html { redirect_to task_path(@task), notice: '投稿できませんでした...' }
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:task_id, :content)
   end
 end
