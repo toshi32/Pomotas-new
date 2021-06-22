92ae46ab8b63e0730df5dfe85001380917147e94
toshi32 <viva.la.vida0522@gmail.com>
2021-06-23 00:35:11 +0900

    追加: コメントコントローラー追加編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,52 @@
 class CommentsController < ApplicationController
+  before_action :set_task, only: [:create, :edit, :update]
+
   def create
-    @task = Task.find(params[:task_id])
-    @comment = @blog.comments.build(comment_params)
+    @comment = @task.comments.build(comment_params)
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to task_path(@task) }
+        format.js { render :index }
       else
         format.html { redirect_to task_path(@task), notice: '投稿できませんでした...' }
       end
     end
   end
 
-  private
+  def edit
+    @comment = @task.comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
 
+  def update
+    @comment = @task.comments.find(params[:id])
+      respond_to do |format|
+        if @comment.update(comment_params)
+          flash.now[:notice] = 'コメントが編集されました'
+          format.js { render :index }
+        else
+          flash.now[:notice] = 'コメントの編集に失敗しました'
+          format.js { render :edit_error }
+        end
+      end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      flash.now[:notice] = 'コメントが削除されました'
+      format.js { render :index }
+    end
+  end
+
+  private
   def comment_params
     params.require(:comment).permit(:task_id, :content)
+  end
+  def set_task
+    @task = Task.find(params[:task_id])
   end
 end
