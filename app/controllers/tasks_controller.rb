8ed8194f13a9e1f9ef82a0eaa819a4e4efbb14e6
toshi32 <viva.la.vida0522@gmail.com>
2021-06-25 11:59:42 +0900

    #30 変更: タスクコントローラー index 整理

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,9 +4,7 @@ class TasksController < ApplicationController
 
   def index
     @q = current_user.tasks.ransack(params[:q])
-    #@tasks = current_user.tasks.all
-    @tasks = @q.result.page(params[:page]).per(5)
-    #@tasks = @search.rescue(distinct: true)
+    @tasks = @q.result.order(created_at: :desc).page(params[:page]).per(5)
   end
 
   def new
