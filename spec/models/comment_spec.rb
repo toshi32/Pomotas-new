c2718de34a7f9ae0884382089c38fc07989c9367
toshi32 <viva.la.vida0522@gmail.com>
2021-06-27 21:16:48 +0900

    追加: コメント機能 Model Spec 記述

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let!(:task) { FactoryBot.create(:task, user_id: user.id) }
+  describe "バリデーションのテスト" do
+    context "コメントが1文字以上の場合", js: true do
+      it "バリデーションが通る" do
+        comment = Comment.new(content: '成功テスト', task: task )
+        expect(comment).to be_valid
+      end
+    end
+    context 'コメントを空欄で登録しようとした場合', js: true do
+      it 'バリデーションにひっかかる' do
+        comment = Comment.new(content: '', task: task )
+        expect(comment).not_to be_valid
+      end
+    end
+  end
 end
