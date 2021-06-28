7ad5742147952c70d88665663ef20b2f565b1dab
toshi32 <viva.la.vida0522@gmail.com>
2021-06-28 22:20:44 +0900

    変更: users showのリダイレクト先をSign Inに設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
     tasks_path
   end
 
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :admin])# サインアップ時にnameのストロングパラメータを追加
